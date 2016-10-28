db7b99f0963dd9fb42394dfd9ef045b660f8dfd9
OleksiiDavydiuk <Oleksii.Davydyuk@gmail.com>
2016-10-28 16:25:49 +0300

    init add

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceProcess;
+
+namespace WindowsFileService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            Service1 myService1 = new Service1();
+            myService1.OnDebag();
+#else
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+        }
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ServiceProcess;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFileService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Time.txt";
+
+            //string desktopPath = AppDomain.CurrentDomain.BaseDirectory + "Time.txt";
+
+           // File.Create(desktopPath);
+
+            do
+            {
+                Thread.Sleep(1200);
+                try
+                {
+                    File.AppendAllText(desktopPath, DateTime.Now + "\n");
+                }
+                catch(Exception ex)
+                {
+                    //
+                }
+                
+            } while (true);
+
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        internal void OnDebag()
+        {
+            this.OnStart(null);
+        }
+    }
+}
